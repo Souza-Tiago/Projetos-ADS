8153b8ed0960b179de0dfec6a24a748f377cec56
Tiago de Jesus Souza <tiagopyerri@gmail.com>
2026-07-14 23:33:41 -0400

    feat: adicionando sistema de interacao com localStorage

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 // Meu primeiro código em ADS!
-const nomeEstudante = "Tiago de Jesus Souza";
-const faculdade = "Anhanguera";
-const curso = "Análise e Desenvolvimento de Sistemas";
+// 1. Mapeamento dos elementos do HTML
+const buttonLike = document.getElementById('btn-like');
+const txtCounter = document.getElementById('counter');
 
-console.log("=========================================");
-console.log(`Olá, Mundo! Sou o ${nomeEstudante}.`);
-console.log(`Estudante de ${curso} na ${faculdade}.`);
-console.log("Ambiente Dev configurado com sucesso! 🚀");
-console.log("=========================================");
+// 2.Busca as curtidas já salvas no navegador, ou começa em 0
+let totalLikes = Number(localStorage.getItem('projeto_likes')) || 0;
+
+// 3. atualizar a tela visualmente
+function renderScreen() {
+    txtCounter.textContent = `${totalLikes} ${totalLikes === 1 ? 'curtida' : 'curtidas'}`;
+}
+
+// 4. Função que processa a lógica de negócio (o clique)
+function handleLike() {
+    totalLikes += 1; // Incrementa o valor
+    localStorage.setItem('projeto_likes', totalLikes); // Salva no banco local do navegador
+    renderScreen(); // Atualiza a tela
+}
+
+// 5. Inicialização: Escuta o clique do botão e desenha o estado inicial na tela
+buttonLike.addEventListener('click', handleLike);
+renderScreen();
