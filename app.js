1e2ae07b236a07a7c039e070cc75bfb4ba46844b
Tiago de Jesus Souza <tiagopyerri@gmail.com>
2026-07-14 23:18:21 -0400

    feat: meu primeiro script javascript executado com sucesso

---
+++ app.js
@@ -0,0 +1,10 @@
+// Meu primeiro código em ADS!
+const nomeEstudante = "Tiago de Jesus Souza";
+const faculdade = "Anhanguera";
+const curso = "Análise e Desenvolvimento de Sistemas";
+
+console.log("=========================================");
+console.log(`Olá, Mundo! Sou o ${nomeEstudante}.`);
+console.log(`Estudante de ${curso} na ${faculdade}.`);
+console.log("Ambiente Dev configurado com sucesso! 🚀");
+console.log("=========================================");
